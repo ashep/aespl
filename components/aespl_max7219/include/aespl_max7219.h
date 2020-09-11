a88370b41e676db313ab6ee5537fe8ae1b9a4818
Alexander Shepetko <a@shepetko.com>
2020-09-11 14:02:46 +0300

    Working on max7219 driver

--- components/aespl_max7219/include/aespl_max7219.h
+++ components/aespl_max7219/include/aespl_max7219.h
@@ -8,55 +8,12 @@
 #ifndef _AESPL_MAX7219_H_
 #define _AESPL_MAX7219_H_
 
+#include "stdbool.h"
 #include "driver/gpio.h"
 
-#define AESPL_MAX7219_POWER_OFF 0x0
-#define AESPL_MAX7219_POWER_ON 0x1
-
-#define AESPL_MAX7219_DECODE_NONE 0x00
-#define AESPL_MAX7219_DECODE_1 0x01
-#define AESPL_MAX7219_DECODE_2 0x0f
-#define AESPL_MAX7219_DECODE_3 0xff
-
-#define AESPL_MAX7219_INTENSITY_MIN 0x0
-#define AESPL_MAX7219_INTENSITY_1 0x1
-#define AESPL_MAX7219_INTENSITY_2 0x2
-#define AESPL_MAX7219_INTENSITY_3 0x3
-#define AESPL_MAX7219_INTENSITY_4 0x4
-#define AESPL_MAX7219_INTENSITY_5 0x5
-#define AESPL_MAX7219_INTENSITY_6 0x6
-#define AESPL_MAX7219_INTENSITY_7 0x7
-#define AESPL_MAX7219_INTENSITY_8 0x8
-#define AESPL_MAX7219_INTENSITY_9 0x9
-#define AESPL_MAX7219_INTENSITY_10 0xa
-#define AESPL_MAX7219_INTENSITY_11 0xb
-#define AESPL_MAX7219_INTENSITY_12 0xc
-#define AESPL_MAX7219_INTENSITY_13 0xd
-#define AESPL_MAX7219_INTENSITY_14 0xe
-#define AESPL_MAX7219_INTENSITY_MAX 0xf
-
-#define AESPL_SCAN_LIMIT_1 0x0
-#define AESPL_SCAN_LIMIT_2 0x1
-#define AESPL_SCAN_LIMIT_3 0x2
-#define AESPL_SCAN_LIMIT_4 0x3
-#define AESPL_SCAN_LIMIT_5 0x4
-#define AESPL_SCAN_LIMIT_6 0x5
-#define AESPL_SCAN_LIMIT_7 0x6
-#define AESPL_SCAN_LIMIT_8 0x7
-
-#define AESPL_TEST_MODE_DISABLE 0
-#define AESPL_TEST_MODE_ENABLE 1
-
 /**
- * Configuration structure
+ * MAX7219 register addresses
  */
-typedef struct {
-    gpio_num_t pin_cs;
-    gpio_num_t pin_clk;
-    gpio_num_t pin_data;
-    uint8_t n_displays;
-} aespl_max7219_config_t;
-
 typedef enum {
     AESPL_MAX7219_ADDR_NOOP,
     AESPL_MAX7219_ADDR_DIGIT_0,
@@ -75,6 +32,79 @@ typedef enum {
 } aespl_max7219_addr_t;
 
 /**
+ * MAX7219 decode modes
+ */
+typedef enum {
+    AESPL_MAX7219_DECODE_NONE = 0x00,
+    AESPL_MAX7219_DECODE_1 = 0x01,
+    AESPL_MAX7219_DECODE_2 = 0x0f,
+    AESPL_MAX7219_DECODE_3 = 0xff,
+} aespl_max7219_decode_mode_t;
+
+/**
+ * MAX7219 intensity modes
+ */
+typedef enum {
+    AESPL_MAX7219_INTENSITY_MIN,
+    AESPL_MAX7219_INTENSITY_1,
+    AESPL_MAX7219_INTENSITY_2,
+    AESPL_MAX7219_INTENSITY_3,
+    AESPL_MAX7219_INTENSITY_4,
+    AESPL_MAX7219_INTENSITY_5,
+    AESPL_MAX7219_INTENSITY_6,
+    AESPL_MAX7219_INTENSITY_7,
+    AESPL_MAX7219_INTENSITY_8,
+    AESPL_MAX7219_INTENSITY_9,
+    AESPL_MAX7219_INTENSITY_10,
+    AESPL_MAX7219_INTENSITY_11,
+    AESPL_MAX7219_INTENSITY_12,
+    AESPL_MAX7219_INTENSITY_13,
+    AESPL_MAX7219_INTENSITY_14,
+    AESPL_MAX7219_INTENSITY_MAX,
+} aespl_max7219_intensity_t;
+
+/**
+ * MAX7219 scan limit modes
+ */
+typedef enum {
+    AESPL_MAX7219_SCAN_LIMIT_1,
+    AESPL_MAX7219_SCAN_LIMIT_2,
+    AESPL_MAX7219_SCAN_LIMIT_3,
+    AESPL_MAX7219_SCAN_LIMIT_4,
+    AESPL_MAX7219_SCAN_LIMIT_5,
+    AESPL_MAX7219_SCAN_LIMIT_6,
+    AESPL_MAX7219_SCAN_LIMIT_7,
+    AESPL_MAX7219_SCAN_LIMIT_8,
+} aespl_max7219_scan_limit_t;
+
+/**
+ * MAX7219 power modes
+ */
+typedef enum {
+    AESPL_MAX7219_POWER_OFF,
+    AESPL_MAX7219_POWER_ON,
+} aespl_max7219_power_mode_t;
+
+/**
+ * MAX7219 test modes
+ */
+typedef enum {
+    AESPL_MAX7219_TEST_MODE_DISABLE,
+    AESPL_MAX7219_TEST_MODE_ENABLE,
+} aespl_max_7219_test_mode_t;
+
+/**
+ * MAX7219 configuration structure
+ */
+typedef struct {
+    gpio_num_t pin_cs;
+    gpio_num_t pin_clk;
+    gpio_num_t pin_data;
+    uint8_t n_displays;
+    aespl_max7219_decode_mode_t decode;
+} aespl_max7219_config_t;
+
+/**
  * @brief Initialize an MCU to work with MAX7219
  * 
  * @param cfg Configuration
@@ -82,32 +112,7 @@ typedef enum {
  *     - ESP_OK Success
  *     - ESP_ERR_INVALID_ARG Parameter error
  */
-esp_err_t
-aespl_max7219_init(const aespl_max7219_config_t *cfg);
-
-/**
- * @brief Send a command to single device
- * 
- * @param cfg Configuration
- * @param addr Address
- * @param data Data
- * @return
- *     - ESP_OK Success
- *     - ESP_ERR_INVALID_ARG GPIO number error
- */
-esp_err_t aespl_max7219_send(const aespl_max7219_config_t *cfg, aespl_max7219_addr_t addr, uint8_t data);
-
-/**
- * @brief Send a command to all devices
- * 
- * @param cfg Configuration
- * @param addr Address
- * @param data Data
- * @return
- *     - ESP_OK Success
- *     - ESP_ERR_INVALID_ARG GPIO number error
- */
-esp_err_t aespl_max7219_send_all(const aespl_max7219_config_t *cfg, aespl_max7219_addr_t addr, uint8_t data);
+esp_err_t aespl_max7219_init(const aespl_max7219_config_t *cfg);
 
 /**
  * @brief Latch sent data into device's registers
@@ -118,5 +123,31 @@ esp_err_t aespl_max7219_send_all(const aespl_max7219_config_t *cfg, aespl_max721
  *     - ESP_ERR_INVALID_ARG GPIO number error
  */
 esp_err_t aespl_max7219_latch(const aespl_max7219_config_t *cfg);
+
+/**
+ * @brief Send a command to single device
+ * 
+ * @param cfg    Configuration
+ * @param addr   Address
+ * @param data   Data
+ * @param latch  Latch
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_ERR_INVALID_ARG GPIO number error
+ */
+esp_err_t aespl_max7219_send(const aespl_max7219_config_t *cfg, aespl_max7219_addr_t addr, uint8_t data, bool latch);
+
+/**
+ * @brief Send a command to all devices
+ * 
+ * @param cfg    Configuration
+ * @param addr   Address
+ * @param data   Data
+ * @param latch  Latch
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_ERR_INVALID_ARG GPIO number error
+ */
+esp_err_t aespl_max7219_send_all(const aespl_max7219_config_t *cfg, aespl_max7219_addr_t addr, uint8_t data);
 
 #endif
--- components/aespl_max7219/max7219.c
+++ components/aespl_max7219/max7219.c
@@ -6,6 +6,7 @@
  */
 
 #include "stdlib.h"
+#include "stdbool.h"
 #include "string.h"
 #include "strings.h"
 #include "freertos/FreeRTOS.h"
@@ -16,6 +17,8 @@
 #include "aespl_max7219.h"
 
 esp_err_t aespl_max7219_init(const aespl_max7219_config_t *cfg) {
+    esp_err_t err;
+
     gpio_config_t gpio_cfg = {
         .pin_bit_mask = BIT(cfg->pin_cs) | BIT(cfg->pin_clk) | BIT(cfg->pin_data),
         .pull_up_en = GPIO_PULLUP_DISABLE,
@@ -24,10 +27,50 @@ esp_err_t aespl_max7219_init(const aespl_max7219_config_t *cfg) {
         .intr_type = GPIO_INTR_DISABLE,
     };
 
-    return gpio_config(&gpio_cfg);
+    err = gpio_config(&gpio_cfg);
+    if (err) {
+        return err;
+    }
+
+    // Set decode mode
+    err = aespl_max7219_send_all(cfg, AESPL_MAX7219_ADDR_DECODE_MODE, cfg->decode);
+    if (err) {
+        return err;
+    }
+
+    // Set minimum intensity
+    err = aespl_max7219_send_all(cfg, AESPL_MAX7219_ADDR_INTENSITY, AESPL_MAX7219_INTENSITY_MIN);
+    if (err) {
+        return err;
+    }    
+
+    // Disable test mode
+    err = aespl_max7219_send_all(cfg, AESPL_MAX7219_ADDR_TEST, AESPL_MAX7219_TEST_MODE_DISABLE);
+    if (err) {
+        return err;
+    }        
+
+    // Power on
+    err = aespl_max7219_send_all(cfg, AESPL_MAX7219_ADDR_POWER, AESPL_MAX7219_POWER_ON);
+    if (err) {
+        return err;
+    }
+
+    return ESP_OK;
 }
 
-esp_err_t aespl_max7219_send(const aespl_max7219_config_t *cfg, aespl_max7219_addr_t addr, uint8_t data) {
+esp_err_t aespl_max7219_latch(const aespl_max7219_config_t *cfg) {
+    esp_err_t err;
+
+    err = gpio_set_level(cfg->pin_cs, 1);
+    if (err) {
+        return err;
+    }
+
+    return gpio_set_level(cfg->pin_cs, 0);
+}
+
+esp_err_t aespl_max7219_send(const aespl_max7219_config_t *cfg, aespl_max7219_addr_t addr, uint8_t data, bool latch) {
     esp_err_t err;
 
     // Setup pins
@@ -55,29 +98,26 @@ esp_err_t aespl_max7219_send(const aespl_max7219_config_t *cfg, aespl_max7219_ad
         gpio_set_level(cfg->pin_clk, 0);
     }
 
-    return ESP_OK;
-}
-
-esp_err_t aespl_max7219_send_all(const aespl_max7219_config_t *cfg, aespl_max7219_addr_t addr, uint8_t data) {
-    esp_err_t err = ESP_OK;
-
-    for (uint8_t i = 0; i < cfg->n_displays; i++) {
-        err = aespl_max7219_send(cfg, addr, data);
+    // Latch
+    if (latch) {
+        err = aespl_max7219_latch(cfg);
         if (err) {
             return err;
         }
     }
 
-    return err;
+    return ESP_OK;
 }
 
-esp_err_t aespl_max7219_latch(const aespl_max7219_config_t *cfg) {
+esp_err_t aespl_max7219_send_all(const aespl_max7219_config_t *cfg, aespl_max7219_addr_t addr, uint8_t data) {
     esp_err_t err;
 
-    err = gpio_set_level(cfg->pin_cs, 1);
-    if (err) {
-        return err;
+    for (uint8_t i = 0; i < cfg->n_displays; i++) {
+        err = aespl_max7219_send(cfg, addr, data, false);
+        if (err) {
+            return err;
+        }
     }
 
-    return gpio_set_level(cfg->pin_cs, 0);
+    return aespl_max7219_latch(cfg);
 }
