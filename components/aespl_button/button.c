b7b84887ff57d373bf591e06968b64e51031786a
Alexander Shepetko <a@shepetko.com>
2020-09-18 13:04:38 +0300

    Button component added

---
+++ components/aespl_button/button.c
@@ -0,0 +1,8 @@
+/**
+ * AESPL Button
+ *
+ * Author: Alexander Shepetko <a@shepetko.com>
+ * License: MIT
+ */
+
+#include "aespl_button.h"
---
+++ components/aespl_button/include/aespl_button.h
@@ -0,0 +1,11 @@
+/**
+ * AESPL Button
+ *
+ * Author: Alexander Shepetko <a@shepetko.com>
+ * License: MIT
+ */
+
+#ifndef AESPL_BUTTON_H
+#define AESPL_BUTTON_H
+
+#endif
